2abefac890d7cb0879183b8899971da5ad42b730
hewenyu <he875027547@126.com>
2023-06-27 23:27:12 +0800

    迁移代码到脚手架运行

---
+++ vue_test/1_迁移项目到脚手架运行/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>测试脚手架代码</h2>
+    <School />
+    <Student />
+    <School />
+  </div>
+</template>
+
+<script>
+import School from "./components/School.vue";
+import Student from "./components/Student.vue";
+
+export default {
+  name: "App",
+  components: {
+    School,
+    Student,
+  },
+};
+</script>
---
+++ vue_test/1_迁移项目到脚手架运行/components/Student.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="student">
+    <h2>学生姓名: {{ name }}</h2>
+    <h2>学生年龄: {{ age }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StudentInfo",
+  data() {
+    return {
+      name: "zhangsan",
+      age: 20,
+    };
+  },
+};
+</script>
+
+<style>
+.student {
+  background-color: aqua;
+}
+</style>
