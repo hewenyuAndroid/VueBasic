b4ec8bb234bab30cdd7c4e38ca21a177e875e04a
hewenyu <he875027547@126.com>
2023-06-29 14:04:50 +0800

    mixin混入使用

---
+++ vue_test/src/components/Student.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="student">
+    <h2>姓名:{{ name }}</h2>
+    <h2>age:{{ age }}</h2>
+    <button @click="showInfo()">点击显示弹窗</button>
+  </div>
+</template>
+
+<script>
+// 局部注册混入
+import { hunru } from "../hunru";
+
+export default {
+  name: "StudentInfo",
+  data() {
+    return {
+      name: "zhangsan",
+    };
+  },
+  mixins: [hunru],
+};
+</script>
+
+<style>
+.student {
+  background-color: bisque;
+}
+</style>
