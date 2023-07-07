e7959568e92edddde83edb33f11b4e9cd97bd7f5
hewenyu <he875027547@126.com>
2023-07-07 22:25:01 +0800

    github搜索案例，使用vue-resouce插件发送请求

---
+++ vue_test/21_src_gihub搜索案例_vue-resource/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <search />
+    <List />
+  </div>
+</template>
+
+<script>
+import List from "./components/List.vue";
+import Search from "./components/Search.vue";
+
+export default {
+  name: "App",
+  components: { List, Search },
+};
+</script>
+
+<style >
+
+</style>
