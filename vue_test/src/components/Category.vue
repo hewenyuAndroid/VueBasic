1e6afad6888bf734a8a92394f2b6e924fd6d79cd
hewenyu <he875027547@126.com>
2023-07-08 19:00:51 +0800

    默认插槽

---
+++ vue_test/src/components/Category.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}分类</h3>
+    <!-- 定义一个插槽 （默认插槽），==>挖个坑，等着组件的使用者填入内容 -->
+    <slot>这是插槽默认显示的内容...</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  data() {},
+  props: ["title"],
+};
+</script>
+
+<style scoped>
+.category {
+  background-color: aqua;
+  width: 200px;
+  height: 300px;
+}
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+/**
+插槽的样式也可以定义在组件中
+*/
+video {
+  width: 100%;
+}
+</style>
