15eb059e10ae7b94eabaacc7d55dc3d14c44ec9e
hewenyu <he875027547@126.com>
2023-07-09 10:06:01 +0800

    求和 纯 vue 版

---
+++ vue_test/23_src_求和_纯vue版/components/Count.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>当前求和为{{ sum }}</h1>
+    <select v-model.number="step">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>&nbsp;
+    <button @click="increament">+</button>&nbsp;
+    <button @click="decreament">-</button>&nbsp;
+    <button @click="increamentOdd">当前求和为奇数再加</button>&nbsp;
+    <button @click="increamentDelay">延迟一会再加</button>&nbsp;
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Count",
+  data() {
+    return {
+      sum: 0, // 当前操作的结果值
+      step: 1, // 用户选择的操作步长
+    };
+  },
+  methods: {
+    increament() {
+      this.sum += this.step;
+    },
+    decreament() {
+      this.sum -= this.step;
+    },
+    increamentOdd() {
+      // 取余为0则为false
+      if (this.sum % 2) {
+        this.sum += this.step;
+      }
+    },
+    increamentDelay() {
+      setTimeout(() => {
+        this.sum += this.step;
+      }, 500);
+    },
+  },
+};
+</script>
