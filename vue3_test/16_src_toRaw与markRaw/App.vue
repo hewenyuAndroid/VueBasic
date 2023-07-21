2fb6e42be64bf44df6843cdc95e597753f966305
hewenyu <he875027547@126.com>
2023-07-21 22:56:21 +0800

    toRaw与markRaw

---
+++ vue3_test/16_src_toRaw与markRaw/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Demo v-if="isShow" />
+  <br />
+  <br />
+  <button @click="isShow = !isShow">隐藏/显示Demo组件</button>
+</template>
+
+<script>
+import Demo from "./components/Demo.vue";
+
+import { ref } from "vue";
+
+export default {
+  name: "App",
+  components: { Demo },
+  setup() {
+    let isShow = ref(true);
+    return { isShow };
+  },
+};
+</script>
+
+<style>
+</style>
